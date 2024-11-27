43a107572059087a52f613b70c932b4e1bbd50de
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2024-11-27 14:45:46 -0300

    banco teste não sei se está funcionando

--- scriptBD.sql
+++ scriptBD.sql
@@ -1,2 +1,14 @@
-create database prolink;
+CREATE DATABASE prolink;
+use prolink;
 
+
+CREATE TABLE inscricoes_webinar
+ (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nome_completo NVARCHAR(100) NOT NULL,
+    email NVARCHAR(100) NOT NULL,
+    telefone NVARCHAR(20),
+    recebe_notificacoes BIT DEFAULT 0,
+    consentimento_lgpd BIT NOT NULL,
+    data_inscricao DATETIME DEFAULT GETDATE()
+);
