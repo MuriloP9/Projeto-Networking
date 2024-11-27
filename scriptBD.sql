780474e458cfd6bef545b40dfb45715845c170a2
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2024-11-27 18:15:25 -0300

    Script do banco atualizado

--- scriptBD.sql
+++ scriptBD.sql
@@ -5,10 +5,11 @@ use prolink;
 CREATE TABLE inscricoes_webinar
  (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    nome_completo NVARCHAR(100) NOT NULL,
-    email NVARCHAR(100) NOT NULL,
-    telefone NVARCHAR(20),
+    nome_completo VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20),
     recebe_notificacoes BIT DEFAULT 0,
     consentimento_lgpd BIT NOT NULL,
     data_inscricao DATETIME DEFAULT GETDATE()
 );
+select*from inscricoes_webinar;
