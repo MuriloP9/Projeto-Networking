91b71e2cba2240edc5b98fc4228dfa428d0afa28
Murilo Pereira <murilopereiranunesrosa@gmail.com>
2024-11-27 21:48:45 -0300

    Script do banco

--- scriptBD.sql
+++ scriptBD.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE prolink;
+
 use prolink;
 
+use master;
+
+drop database prolink;
 
 CREATE TABLE inscricoes_webinar
  (
@@ -12,4 +16,17 @@ CREATE TABLE inscricoes_webinar
     consentimento_lgpd BIT NOT NULL,
     data_inscricao DATETIME DEFAULT GETDATE()
 );
+
 select*from inscricoes_webinar;
+
+
+CREATE TABLE cadastro (
+    id INT IDENTITY(1,1) PRIMARY KEY, -- ID único e auto-incrementado
+    nome VARCHAR(255) NOT NULL,         -- Nome completo
+    email VARCHAR(255) NOT NULL UNIQUE, -- Email único
+    senha VARCHAR(255) NOT NULL,        -- Senha (será salva de forma segura, idealmente criptografada)
+    dataNascimento DATE NOT NULL,      -- Data de nascimento
+    telefone VARCHAR(20) NOT NULL       -- Telefone
+);
+
+select * from cadastro;
